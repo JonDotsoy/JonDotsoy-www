baae99e51d27076b56eeeb2056bbac253e4745cf
Jonathan Delgado <hi@jon.soy>
2017-05-17 21:48:13 -0400

    Added favicons

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ gulp.task('styles:watch', ['styles'], () => {
   gulp.watch(['src/styles/**/*.css'], ['styles'])
 })
 
-gulp.task('styles', () =>
+gulp.task('styles', () => (
   gulp.src(['src/styles/**/*.css'])
   .pipe(require('gulp-postcss')([
     require('postcss-import')(),
@@ -12,24 +12,31 @@ gulp.task('styles', () =>
     require('postcss-cssnext')(),
   ]))
   .pipe(gulp.dest('dest/styles'))
-)
+))
 
 gulp.task('templates:watch', ['templates'], () => {
   gulp.watch(['src/templates/**/*.pug'], ['templates'])
 })
 
-gulp.task('templates', () =>
+gulp.task('templates', () => (
   gulp.src(['src/templates/*.pug'])
   .pipe(require('gulp-pug')())
   .pipe(gulp.dest('dest'))
-)
+))
+
+gulp.task('copy-favicons', () => (
+  gulp.src(['src/favicons/**/*'])
+  .pipe(gulp.dest('dest'))
+))
 
 gulp.task('watch', [
+  'copy-favicons',
   'styles:watch',
   'templates:watch',
 ])
 
 gulp.task('build', [
+  'copy-favicons',
   'styles',
   'templates',
 ])
